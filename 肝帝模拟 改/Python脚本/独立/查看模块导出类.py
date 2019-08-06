488565eb97ad2fa364964db16ef1f02e01d734b2
Kana <orzfirst@vip.qq.com>
2019-08-05 18:09:20 -0700

    1.3.0.0

---
+++ 肝帝模拟 改/Python脚本/独立/查看模块导出类.py
@@ -0,0 +1,13 @@
+# 查看模块里都有什么东西能在python脚本里用
+
+import KancollePlayerSimulatorKaiCore
+import KancollePlayerSimulatorKai
+
+def print_all(module_):
+	modulelist = dir(module_)
+	length = len(modulelist)
+	for i in range(0,length,1):
+		print getattr(module_,modulelist[i])
+
+print_all(KancollePlayerSimulatorKaiCore)
+print_all(KancollePlayerSimulatorKai)
